5ca9ce1fc6a40c41d88b06d4ce7093f439be5900
NkotanyiM <gahamire12@gmail.com>
2025-05-19 13:17:20 +0200

    Update del.php

--- del.php
+++ del.php
@@ -1,7 +1,7 @@
 <?php
 $conn=mysqli_connect('localhost','root','','employee');
-$id=$_GET['emp_id'];
-$delete="DELETE FROM employees WHERE emp_id='$id'";
+$id=$_GET['id'];
+$delete="DELETE FROM employees WHERE id='$id'";
 if (mysqli_query($conn,$delete)) {
         echo"<script>alert('Candidate is deleted!!');
         location.href='employees.php'
